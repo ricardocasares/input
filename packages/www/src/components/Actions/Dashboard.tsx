0746a30248d84d500f5f059501350fac6540f343
Ricardo Casares <ricardocasares@gmail.com>
2019-06-25 02:16:33 +0200

    unique ids

--- packages/www/src/components/Actions/Dashboard.tsx
+++ packages/www/src/components/Actions/Dashboard.tsx
@@ -4,7 +4,7 @@ export const Dashboard = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 853.5 784">
     <defs>
       <linearGradient
-        id="a"
+        id="dashboard-a"
         x1="426.8"
         x2="426.8"
         y1="651.1"
@@ -15,42 +15,222 @@ export const Dashboard = () => (
         <stop offset=".5" stopColor="gray" stopOpacity=".1" />
         <stop offset="1" stopColor="gray" stopOpacity=".1" />
       </linearGradient>
-      <linearGradient id="b" x1="263.5" x2="263.5" y1="255.6" y2="205.1" />
-      <linearGradient id="c" x1="129.5" x2="129.5" y1="323.6" y2="273.1" />
-      <linearGradient id="d" x1="415.5" x2="415.5" y1="392.6" y2="342.1" />
-      <linearGradient id="e" x1="212.5" x2="212.5" y1="460.6" y2="410.1" />
-      <linearGradient id="f" x1="81.5" x2="81.5" y1="528.6" y2="478.1" />
-      <linearGradient id="g" x1="346.5" x2="346.5" y1="596.6" y2="546.1" />
-      <linearGradient id="h" x1="577.2" x2="577.2" y1="453" y2="406.7" />
-      <linearGradient id="i" x1="669" x2="669" y1="453" y2="406.7" />
-      <linearGradient id="j" x1="760.7" x2="760.7" y1="453" y2="406.7" />
-      <linearGradient id="k" x1="577.2" x2="577.2" y1="523.5" y2="477.1" />
-      <linearGradient id="l" x1="669" x2="669" y1="523.5" y2="477.1" />
-      <linearGradient id="m" x1="760.7" x2="760.7" y1="523.5" y2="477.1" />
-      <linearGradient id="n" x1="577.2" x2="577.2" y1="593.9" y2="547.5" />
-      <linearGradient id="o" x1="669" x2="669" y1="593.9" y2="547.5" />
-      <linearGradient id="p" x1="760.7" x2="760.7" y1="593.9" y2="547.5" />
-      <linearGradient id="q" x1="232.6" x2="586.1" y1="760" y2="760" />
-      <linearGradient id="r" x1="693.5" x2="693.5" y1="104.5" y2="34" />
-      <linearGradient id="s" x1="866.7" x2="866.7" y1="131.9" y2="122.6" />
-      <linearGradient id="t" x1="622" x2="622" y1="104.5" y2="34" />
-      <linearGradient id="u" x1="795.3" x2="795.3" y1="131.9" y2="122.6" />
-      <linearGradient id="v" x1="651.9" x2="651.9" y1="101.3" y2="30.8" />
-      <linearGradient id="w" x1="825.1" x2="825.1" y1="128.7" y2="119.4" />
-      <linearGradient id="x" x1="617.5" x2="617.5" y1="266" y2="187.8" />
-      <linearGradient id="y" x1="720.5" x2="720.5" y1="266" y2="187.8" />
-      <linearGradient id="z" x1="617.5" x2="617.5" y1="358" y2="279.8" />
-      <linearGradient id="A" x1="720.5" x2="720.5" y1="358" y2="279.8" />
+      <linearGradient
+        id="dashboard-b"
+        x1="263.5"
+        x2="263.5"
+        y1="255.6"
+        y2="205.1"
+      />
+      <linearGradient
+        id="dashboard-c"
+        x1="129.5"
+        x2="129.5"
+        y1="323.6"
+        y2="273.1"
+      />
+      <linearGradient
+        id="dashboard-d"
+        x1="415.5"
+        x2="415.5"
+        y1="392.6"
+        y2="342.1"
+      />
+      <linearGradient
+        id="dashboard-e"
+        x1="212.5"
+        x2="212.5"
+        y1="460.6"
+        y2="410.1"
+      />
+      <linearGradient
+        id="dashboard-f"
+        x1="81.5"
+        x2="81.5"
+        y1="528.6"
+        y2="478.1"
+      />
+      <linearGradient
+        id="dashboard-g"
+        x1="346.5"
+        x2="346.5"
+        y1="596.6"
+        y2="546.1"
+      />
+      <linearGradient
+        id="dashboard-h"
+        x1="577.2"
+        x2="577.2"
+        y1="453"
+        y2="406.7"
+      />
+      <linearGradient id="dashboard-i" x1="669" x2="669" y1="453" y2="406.7" />
+      <linearGradient
+        id="dashboard-j"
+        x1="760.7"
+        x2="760.7"
+        y1="453"
+        y2="406.7"
+      />
+      <linearGradient
+        id="dashboard-k"
+        x1="577.2"
+        x2="577.2"
+        y1="523.5"
+        y2="477.1"
+      />
+      <linearGradient
+        id="dashboard-l"
+        x1="669"
+        x2="669"
+        y1="523.5"
+        y2="477.1"
+      />
+      <linearGradient
+        id="dashboard-m"
+        x1="760.7"
+        x2="760.7"
+        y1="523.5"
+        y2="477.1"
+      />
+      <linearGradient
+        id="dashboard-n"
+        x1="577.2"
+        x2="577.2"
+        y1="593.9"
+        y2="547.5"
+      />
+      <linearGradient
+        id="dashboard-o"
+        x1="669"
+        x2="669"
+        y1="593.9"
+        y2="547.5"
+      />
+      <linearGradient
+        id="dashboard-p"
+        x1="760.7"
+        x2="760.7"
+        y1="593.9"
+        y2="547.5"
+      />
+      <linearGradient
+        id="dashboard-q"
+        x1="232.6"
+        x2="586.1"
+        y1="760"
+        y2="760"
+      />
+      <linearGradient
+        id="dashboard-r"
+        x1="693.5"
+        x2="693.5"
+        y1="104.5"
+        y2="34"
+      />
+      <linearGradient
+        id="dashboard-s"
+        x1="866.7"
+        x2="866.7"
+        y1="131.9"
+        y2="122.6"
+      />
+      <linearGradient id="dashboard-t" x1="622" x2="622" y1="104.5" y2="34" />
+      <linearGradient
+        id="dashboard-u"
+        x1="795.3"
+        x2="795.3"
+        y1="131.9"
+        y2="122.6"
+      />
+      <linearGradient
+        id="dashboard-v"
+        x1="651.9"
+        x2="651.9"
+        y1="101.3"
+        y2="30.8"
+      />
+      <linearGradient
+        id="dashboard-w"
+        x1="825.1"
+        x2="825.1"
+        y1="128.7"
+        y2="119.4"
+      />
+      <linearGradient
+        id="dashboard-x"
+        x1="617.5"
+        x2="617.5"
+        y1="266"
+        y2="187.8"
+      />
+      <linearGradient
+        id="dashboard-y"
+        x1="720.5"
+        x2="720.5"
+        y1="266"
+        y2="187.8"
+      />
+      <linearGradient
+        id="dashboard-z"
+        x1="617.5"
+        x2="617.5"
+        y1="358"
+        y2="279.8"
+      />
+      <linearGradient
+        id="dashboard-A"
+        x1="720.5"
+        x2="720.5"
+        y1="358"
+        y2="279.8"
+      />
     </defs>
-    <path fill="url(#a)" d="M0 132.9h853.5v518.2H0z" opacity=".5" />
+    <path fill="url(#dashboard-a)" d="M0 132.9h853.5v518.2H0z" opacity=".5" />
     <path fill="#fff" d="M7.5 141.8h838.6v500.4H7.5z" />
     <path fill="#f5f5f5" d="M45.9 222.9h436.4v14.9H45.9z" />
-    <circle cx="263.5" cy="230.4" r="25.2" fill="url(#b)" opacity=".5" />
-    <circle cx="129.5" cy="298.4" r="25.2" fill="url(#c)" opacity=".5" />
-    <circle cx="415.5" cy="367.4" r="25.2" fill="url(#d)" opacity=".5" />
-    <circle cx="212.5" cy="435.4" r="25.2" fill="url(#e)" opacity=".5" />
-    <circle cx="81.5" cy="503.4" r="25.2" fill="url(#f)" opacity=".5" />
-    <circle cx="346.5" cy="571.4" r="25.2" fill="url(#g)" opacity=".5" />
+    <circle
+      cx="263.5"
+      cy="230.4"
+      r="25.2"
+      fill="url(#dashboard-b)"
+      opacity=".5"
+    />
+    <circle
+      cx="129.5"
+      cy="298.4"
+      r="25.2"
+      fill="url(#dashboard-c)"
+      opacity=".5"
+    />
+    <circle
+      cx="415.5"
+      cy="367.4"
+      r="25.2"
+      fill="url(#dashboard-d)"
+      opacity=".5"
+    />
+    <circle
+      cx="212.5"
+      cy="435.4"
+      r="25.2"
+      fill="url(#dashboard-e)"
+      opacity=".5"
+    />
+    <circle
+      cx="81.5"
+      cy="503.4"
+      r="25.2"
+      fill="url(#dashboard-f)"
+      opacity=".5"
+    />
+    <circle
+      cx="346.5"
+      cy="571.4"
+      r="25.2"
+      fill="url(#dashboard-g)"
+      opacity=".5"
+    />
     <circle cx="263.5" cy="230.4" r="19.2" fill="#6c63ff" opacity=".5" />
     <path fill="#f5f5f5" d="M45.9 291.2h436.4v14.9H45.9z" />
     <circle cx="129.1" cy="298.6" r="19.2" fill="#6c63ff" opacity=".5" />
@@ -62,15 +242,51 @@ export const Dashboard = () => (
     <circle cx="81.1" cy="503.5" r="19.2" fill="#6c63ff" opacity=".5" />
     <path fill="#f5f5f5" d="M45.9 564.3h436.4v14.9H45.9z" />
     <circle cx="346.8" cy="571.8" r="19.2" fill="#6c63ff" />
-    <path fill="url(#h)" d="M543.1 406.7h68.3V453h-68.3z" opacity=".5" />
-    <path fill="url(#i)" d="M634.8 406.7h68.3V453h-68.3z" opacity=".5" />
-    <path fill="url(#j)" d="M726.6 406.7h68.3V453h-68.3z" opacity=".5" />
-    <path fill="url(#k)" d="M543.1 477.1h68.3v46.3h-68.3z" opacity=".5" />
-    <path fill="url(#l)" d="M634.8 477.1h68.3v46.3h-68.3z" opacity=".5" />
-    <path fill="url(#m)" d="M726.6 477.1h68.3v46.3h-68.3z" opacity=".5" />
-    <path fill="url(#n)" d="M543.1 547.5h68.3v46.3h-68.3z" opacity=".5" />
-    <path fill="url(#o)" d="M634.8 547.5h68.3v46.3h-68.3z" opacity=".5" />
-    <path fill="url(#p)" d="M726.6 547.5h68.3v46.3h-68.3z" opacity=".5" />
+    <path
+      fill="url(#dashboard-h)"
+      d="M543.1 406.7h68.3V453h-68.3z"
+      opacity=".5"
+    />
+    <path
+      fill="url(#dashboard-i)"
+      d="M634.8 406.7h68.3V453h-68.3z"
+      opacity=".5"
+    />
+    <path
+      fill="url(#dashboard-j)"
+      d="M726.6 406.7h68.3V453h-68.3z"
+      opacity=".5"
+    />
+    <path
+      fill="url(#dashboard-k)"
+      d="M543.1 477.1h68.3v46.3h-68.3z"
+      opacity=".5"
+    />
+    <path
+      fill="url(#dashboard-l)"
+      d="M634.8 477.1h68.3v46.3h-68.3z"
+      opacity=".5"
+    />
+    <path
+      fill="url(#dashboard-m)"
+      d="M726.6 477.1h68.3v46.3h-68.3z"
+      opacity=".5"
+    />
+    <path
+      fill="url(#dashboard-n)"
+      d="M543.1 547.5h68.3v46.3h-68.3z"
+      opacity=".5"
+    />
+    <path
+      fill="url(#dashboard-o)"
+      d="M634.8 547.5h68.3v46.3h-68.3z"
+      opacity=".5"
+    />
+    <path
+      fill="url(#dashboard-p)"
+      d="M726.6 547.5h68.3v46.3h-68.3z"
+      opacity=".5"
+    />
     <path
       fill="#6c63ff"
       d="M547.3 409.6h59.8v40.5h-59.8zM639.1 409.6h59.8v40.5h-59.8z"
@@ -85,7 +301,7 @@ export const Dashboard = () => (
     <path fill="#6c63ff" d="M639.1 550.4h59.8v40.5h-59.8z" opacity=".5" />
     <path fill="#3ad29f" d="M730.9 550.4h59.8v40.5h-59.8z" opacity=".5" />
     <path
-      fill="url(#q)"
+      fill="url(#dashboard-q)"
       d="M546.6 741.8c-6 0-10.4 1-13.6 6.5H404.4v-13.8c0-3.6-4.6-2.3-7.6-2.3h-2.5c-3 0-5.5-1.4-5.5 2.3v3H244.3c-7.5 0-10.5 6.6-11.5 15.1v-.6l-.2 1.8v9.4c0 9.7 3.5 17.6 11.7 17.6h144.5v4.6c0 3.6 2.4 2.3 5.5 2.3h2.5c3 0 7.6 1.3 7.6-2.3v-13.6h129.4a20 20 0 0 0 2 2.4c5.5 2.8 5.6 3.4 10.7 3.4L586 776v-32z"
       opacity=".5"
       transform="translate(-173.2 -58)"
@@ -121,11 +337,11 @@ export const Dashboard = () => (
       opacity=".1"
     />
     <path
-      fill="url(#r)"
+      fill="url(#dashboard-r)"
       d="M693.5 34L666 49.9V88.6l27.5 15.9L721 88.6V49.9L693.5 34z"
     />
     <path
-      fill="url(#s)"
+      fill="url(#dashboard-s)"
       d="M866.7 131.9a8 8 0 0 1-7.3-4.7 8 8 0 0 1 14.6 0 8 8 0 0 1-7.3 4.7z"
       opacity=".5"
       transform="translate(-173.2 -58)"
@@ -139,11 +355,11 @@ export const Dashboard = () => (
       d="M693.5 36.5L668 51.3v29.5l25.6 14.8 25.6-14.8V51.3zm0 37A7.5 7.5 0 1 1 701 66a7.5 7.5 0 0 1-7.5 7.5z"
     />
     <path
-      fill="url(#t)"
+      fill="url(#dashboard-t)"
       d="M622 34l-27.5 15.9V88.6l27.5 15.9 27.5-15.9V49.9L622 34z"
     />
     <path
-      fill="url(#u)"
+      fill="url(#dashboard-u)"
       d="M795.3 131.9a8 8 0 0 1-7.3-4.7 8 8 0 0 1 14.6 0 8 8 0 0 1-7.4 4.7z"
       opacity=".5"
       transform="translate(-173.2 -58)"
@@ -157,11 +373,11 @@ export const Dashboard = () => (
       d="M622 36.5l-25.6 14.8v29.5L622 95.6l25.6-14.8V51.3zm0 37a7.5 7.5 0 1 1 7.5-7.5 7.5 7.5 0 0 1-7.5 7.5z"
     />
     <path
-      fill="url(#v)"
+      fill="url(#dashboard-v)"
       d="M651.9 30.8l-27.5 15.9V85.4l27.5 15.9 27.5-15.9V46.7l-27.5-15.9z"
     />
     <path
-      fill="url(#w)"
+      fill="url(#dashboard-w)"
       d="M825.1 128.7a8 8 0 0 1-7.3-4.7 8 8 0 0 1 14.6 0 8 8 0 0 1-7.3 4.7z"
       opacity=".5"
       transform="translate(-173.2 -58)"
@@ -214,7 +430,7 @@ export const Dashboard = () => (
       strokeWidth="3"
       opacity=".5"
     />
-    <circle cx="617.5" cy="226.9" r="39.1" fill="url(#x)" />
+    <circle cx="617.5" cy="226.9" r="39.1" fill="url(#dashboard-x)" />
     <circle cx="617.5" cy="226.9" r="36.7" fill="#fff" />
     <circle cx="617.5" cy="226.9" r="2.5" fill="#333" />
     <path
@@ -228,7 +444,7 @@ export const Dashboard = () => (
       d="M623.8 226a36.6 36.6 0 0 0 16.5 30.5 36.7 36.7 0 0 0-2-59.7 36.6 36.6 0 0 0-14.5 29.1z"
       opacity=".1"
     />
-    <circle cx="720.5" cy="226.9" r="39.1" fill="url(#y)" />
+    <circle cx="720.5" cy="226.9" r="39.1" fill="url(#dashboard-y)" />
     <circle cx="720.5" cy="226.9" r="36.7" fill="#fff" />
     <circle cx="720.5" cy="226.9" r="2.5" fill="#333" />
     <path
@@ -242,7 +458,7 @@ export const Dashboard = () => (
       d="M726.8 226a36.6 36.6 0 0 0 16.5 30.5 36.7 36.7 0 0 0-2-59.7 36.6 36.6 0 0 0-14.5 29.1z"
       opacity=".1"
     />
-    <circle cx="617.5" cy="318.9" r="39.1" fill="url(#z)" />
+    <circle cx="617.5" cy="318.9" r="39.1" fill="url(#dashboard-z)" />
     <circle cx="617.5" cy="318.9" r="36.7" fill="#fff" />
     <circle cx="617.5" cy="318.9" r="2.5" fill="#333" />
     <path
@@ -256,7 +472,7 @@ export const Dashboard = () => (
       d="M623.8 318a36.6 36.6 0 0 0 16.5 30.5 36.7 36.7 0 0 0-2-59.7 36.6 36.6 0 0 0-14.5 29.1z"
       opacity=".1"
     />
-    <circle cx="720.5" cy="318.9" r="39.1" fill="url(#A)" />
+    <circle cx="720.5" cy="318.9" r="39.1" fill="url(#dashboard-A)" />
     <circle cx="720.5" cy="318.9" r="36.7" fill="#fff" />
     <circle cx="720.5" cy="318.9" r="2.5" fill="#333" />
     <path
